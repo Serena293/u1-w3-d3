b8dae2a4ed4237ad6f048031180ca07c09529909
SERENA FERRARO <serenaferraro21@gmail.com>
2024-10-16 22:57:30 +0200

    correction

--- scripts/index.js
+++ scripts/index.js
@@ -2,23 +2,27 @@ const task = document.getElementsByTagName("form")[0];
 const inputTask = document.getElementById("toDoInput");
 const taskLabel = document.getElementsByTagName("label")[0];
 const taskBtn = document.getElementById("taskBtn");
-const list = document.getElementById("list");
+const container = document.getElementById("container");
 
 const addNewTask = (e) => {
   e.preventDefault();
 
+  const list = document.createElement('article')
   const newTask = {
     todo: inputTask.value,
   };
 
   list.classList.add('newListItem')
+  
 
-  list.innerHTML = ` <ul></ul><button></button> `;
+  list.innerHTML = ` <ul></ul><button></button><button></button> `;
+  container.appendChild('list')
 
-  const crossBtn = document.querySelector("div button");
-  crossBtn.classList = "crossBtn";
+  const crossBtn = document.getElementsByTagName('button')[2]
+  const deleteBtn = document.getElementsByTagName('button')[1]
+  crossBtn.classList.add('crossBtn')
   crossBtn.innerText = "Done";
-
+deleteBtn.innerText = 'Delete'
   const listItems = document.getElementsByTagName("ul")[0];
 
   const listElement = document.createElement('li')
@@ -28,6 +32,7 @@ const addNewTask = (e) => {
 
   task.reset();
 
+  deleteBtn.addEventListener('click', function(){list.remove()})
   crossBtn.addEventListener("click", function () {
     listElement.style.textDecoration = "line-through";
   });
--- scripts/newscript.js
+++ scripts/newscript.js
@@ -1,15 +1,34 @@
-const task = document.getElementsByTagName("form")[0];
-const inputTask = document.getElementById("toDoInput");
-const taskLabel = document.getElementsByTagName("label")[0];
 const taskBtn = document.getElementById("taskBtn");
+const input = document.getElementById("toDoInput");
+const container = document.getElementById("container");
 const list = document.getElementById("list");
 
-const addNewTask = (e) => {
+const addTask = (e) => {
     e.preventDefault();
+  
+    let newInput = input.value
 
-    
+    const newListItem = document.createElement("li");
+    newListItem.innerHTML = `
+        <p>${newInput}</p>
+        <button class='doneBtn'>Done</button>
+        <button class='deleteBtn'>Delete</button>
+    `;
 
-}
+    const doneBtn = newListItem.getElementsByClassName('doneBtn')[0];
+    const deleteBtn = newListItem.getElementsByClassName('deleteBtn')[0];
 
+    doneBtn.addEventListener('click', () => {
+        newListItem.querySelector('p').style.textDecoration = "line-through";
+    });
 
-    taskBtn.addEventListener("click", addNewTask);
+    deleteBtn.addEventListener('click', () => {
+        list.removeChild(newListItem);
+    });
+
+    list.appendChild(newListItem); 
+   
+    input.value = "";  
+};
+
+taskBtn.addEventListener("click", addTask);
